95ddd43a5b95a10d27394f0528e84787dced2556
faragobruno <farago.bruno@gmail.com>
2021-03-17 19:18:11 +0100

    Frontend Vue alapja

---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-app id="inspire">
+      <v-card>
+          <v-card-text>
+              <p>Welocme</p>
+          </v-card-text>
+      </v-card>
+  </v-app>
+</template>
+
+<script>
+export default {
+    name: 'Home'
+}
+</script>
+
+<style>
+
+</style>
