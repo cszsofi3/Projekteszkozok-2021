4b03eef734f0143c8ac4647d2d8581d1a1efcb07
Csákány Zsófia <g41i7l@inf.elte.hu>
2021-03-26 21:08:25 +0100

    fixing data.sql and webconfig

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -1,6 +1,6 @@
-INSERT INTO user(name, basket_price) VALUES('George', 0);
-INSERT INTO user(name, basket_price) VALUES('Kate', 0);
-INSERT INTO user(name, basket_price) VALUES('Lotte', 0);
+INSERT INTO users(name, basket_price) VALUES('George', 0);
+INSERT INTO users(name, basket_price) VALUES('Kate', 0);
+INSERT INTO users(name, basket_price) VALUES('Lotte', 0);
 
 INSERT INTO FOOD(name, type, price) VALUES('Margherita', 'PIZZA', 1500);
 INSERT INTO FOOD(name, type, price) VALUES('Cheese', 'HAMBURGER', 2000);
@@ -19,6 +19,6 @@ INSERT INTO FOOD_INGREDIENT(food_id, ingredient_id) VALUES(1,3);
 
 -- INSERT INTO BASKET_FOOD_EXTRA_INGREDIENT(basket_food_id, extra_ingredient_id) VALUES(1,1);
 
-INSERT INTO USER_BASKET(user_id, basket_id) VALUES(1,1);
+-- INSERT INTO USER_BASKET(user_id, basket_id) VALUES(1,1);
 
 -- INSERT INTO BASKET_FOOD(basket_id, food_id) VALUES(1,1);
